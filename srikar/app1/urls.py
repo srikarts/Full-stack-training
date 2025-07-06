80881f90865146f0a49fa620b1a28b2630bf1363
srikar_ts <122414947+srikarts@users.noreply.github.com>
2025-07-06 16:01:45 +0530

    Delete srikar directory

--- srikar/app1/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('home/',views.home,name='home'),
-    path('',views.page,name='page'),
-    path('demo/',views.demo,name='demo'),
-]
